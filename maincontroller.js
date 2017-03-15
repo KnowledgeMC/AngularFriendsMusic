3ce5a2bced53b25e16cd17cbab1362bc5586b9d6
Roy Hwang <tupaclypse@gmail.com>
2017-03-15 15:15:44 -0700

    Added CSS file, linked

--- maincontroller.js
+++ maincontroller.js
@@ -1,1 +1,7 @@
-app.controller("MainController", function($scope){ });
+//$scope variable added as paramter to controller function
+//this is where we assign all our controller variables and make them
+//available within the #content DIV
+app.controller("MainController", function($scope){
+  $scope.understand = "I now understand how the scope works!";
+});
+
